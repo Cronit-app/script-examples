91b26daec2c4016dfd6d5814f263a5f3a408f5de
Alexis Clarembeau <alexis.clarembeau@gmail.com>
2022-08-31 06:40:56 +0200

    Create sample-http-call.js

---
+++ sample-http-call.js
@@ -0,0 +1,9 @@
+/**
+  Sample JavaScript code for Cronit online cronjobs scripting system 
+  To run on https://cronit.app 
+  
+  Example: How to run HTTP code 
+*/
+module.exports = async () => {
+    let {data, status} = await http.get('https://your-webhook-endpoint')  
+}
